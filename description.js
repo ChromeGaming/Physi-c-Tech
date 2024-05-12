57465e89b6e9de21522f47b5a85bcacd3c2f668a
SIM_RAITANI <simranraitani53@gmail.com>
2024-05-12 23:12:04 +0530

    FIX #11 dialog box added

---
+++ description.js
@@ -0,0 +1,25 @@
+const speechTexts = [
+    "Hello, tech-enthusiasts!",
+    "Welcome to our game!",
+    "Are you ready to embark on an epic journey of technology and physics?",
+    "Drag and drop the icon into the empty area to form new physics terms.",
+    "Click on the hint button above if you get stuck.",
+    "Happy playing!!"
+];
+
+let speechIndex = 0;
+
+function nextSpeech() {
+    if (speechIndex==speechTexts.length){
+        speechIndex=0;
+        cross();
+    }
+    else{
+        document.getElementById('speech-text').textContent = speechTexts[speechIndex];
+        speechIndex = (speechIndex + 1);
+    }
+}
+function cross(){
+    document.querySelector(".pop-up").style.display="none";
+    document.querySelector(".outer").style.filter="blur(0px)";
+}
